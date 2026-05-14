6303def2ae52180f77a38565dcaf1e7c265de8f5
sumin <zxcd0524@naver.com>
2026-05-14 10:28:42 +0900

    Initial Commit

---
+++ src/test/java/luma/example/luma_scan/LumaScanApplicationTests.java
@@ -0,0 +1,13 @@
+package luma.example.luma_scan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LumaScanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
